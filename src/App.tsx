7d0909c5adf9ba02306d72f91d8a3d4a77d3724d
younsang-dpsl <younsang.na@deepsales.com>
2025-01-17 18:23:27 +0900

    feat: table 기본 페이지네이션 구현
    무한 스크롤이 아닌, 제한적 페이지 사이즈만큼 각 페이지에 해당하는 데이터를 보여준다.
    테이블의 getState()와 페이지 제어 상태 로직에 대해 학습했다.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
+  PaginationState,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -59,6 +61,11 @@ function generateMockUsers(count: number): User[] {
 const mockData = generateMockUsers(1000);
 
 function App() {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
   const columns: ColumnDef<User>[] = useMemo(
     () => [
       {
@@ -95,38 +102,82 @@ function App() {
     data: mockData,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    //no need to pass pageCount or rowCount with client-side pagination as it is calculated automatically
+    state: {
+      pagination,
+    },
   });
 
   return (
-    <table>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <main>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{ display: "flex", gap: "32px" }}>
+        <button
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          PREV
+        </button>
+        <button
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          NEXT
+        </button>
+        <label>
+          Go to Page:
+          <input
+            type="number"
+            placeholder="set Page.."
+            min={0}
+            max={table.getPageCount() - 1}
+            value={table.getState().pagination.pageIndex}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) : 0;
+              table.setPageIndex(page);
+            }}
+          />
+        </label>
+      </div>
+      <div>
+        Showing{" "}
+        {(
+          table.getRowModel().rows.length *
+          (table.getState().pagination.pageIndex + 1)
+        ).toLocaleString()}{" "}
+        of {table.getRowCount().toLocaleString()} Data
+      </div>
+    </main>
   );
 }
 
